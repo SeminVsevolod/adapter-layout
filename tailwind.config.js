61bcbbee4cc897b8c6c17b402eb02c6a2d4fb26e
SeminVsevolod <semin.vsevolod@gmail.com>
2020-08-06 20:19:23 +0300

    вёрстка под десктоп

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '10xl': '8rem'
+      },
+      flex: {
+        '2': '2 2 0%',
+        '3': '3 3 0%',
+      }
+    },
   },
   variants: {},
   plugins: [],
